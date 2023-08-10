904ce1ff5b578d1b01d906cbf378e124b4b0314c
1MishGun1 <mikhail.kudrov@gmail.com>
2023-08-10 23:45:51 +0300

    new test version

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-'use strict';
+"use strict";
 
-console.log(document.querySelector('.guess-message').textContent);
+// console.log(document.querySelector('.guess-message').textContent);
 
-document.querySelector('.guess-message').textContent =
-  'Well done! Correct answer :)';
+// document.querySelector('.guess-message').textContent =
+//   'Well done! Correct answer :)';
 
-document.querySelector('.question').textContent = 7;
+// document.querySelector('.question').textContent = 7;
 
-document.querySelector('.score').textContent = 11;
+// document.querySelector('.score').textContent = 11;
 
-console.log(document.querySelector('.number-input').value);
-document.querySelector('.number-input').value = 12;
-console.log(document.querySelector('.number-input').value);
+// console.log(document.querySelector('.number-input').value);
+// document.querySelector('.number-input').value = 12;
+// console.log(document.querySelector('.number-input').value);
+
+// const eventHandler = function () {
+//   console.log(document.querySelector(".number-input").value);
+// };
+
+document.querySelector(".check").addEventListener("click", function () {
+  const gussingNumber = Number(document.querySelector(".number-input").value);
+  console.log(gussingNumber, typeof gussingNumber);
+
+  if (!gussingNumber) {
+    document.querySelector(".guess-message").textContent = "Enter a number";
+  }
+});
